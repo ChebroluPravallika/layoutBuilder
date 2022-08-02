72071b80085ea11600e17ad30d19b1d3e87af270
ChebroluPravallika <95267108+ChebroluPravallika@users.noreply.github.com>
2022-08-02 19:39:19 +0530

    Update index.js

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -13,14 +13,15 @@ const Body = () => (
             <div
               style={{
                 marginTop: '30px',
-
                 backgroundColor: '#e2e8f0',
                 width: '32%',
-                padding: '20px',
               }}
             >
-              <h1 className="body-header-text">Left Navbar Menu</h1>
-              <ul className="list-items">
+              <h1 className="header">Left Navbar Menu</h1>
+              <ul
+                className="list-items"
+                style={{listStyle: 'none', textAlign: 'center'}}
+              >
                 <li className="item">Item 1</li>
                 <li className="item">Item 2</li>
                 <li className="item">Item 3</li>
@@ -36,7 +37,7 @@ const Body = () => (
                 padding: '20px',
               }}
             >
-              <h1 className="body-header-text">Content</h1>
+              <h1 className="header">Content</h1>
               <p className="content-text">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -54,10 +55,10 @@ const Body = () => (
                 padding: '20px',
               }}
             >
-              <h1 className="body-header-text">Right Navbar</h1>
-              <div className="ad-box-container">
-                <p className="ad-box">Ad 1</p>
-                <p className="ad-box">Ad 2</p>
+              <h1 className="header">Right Navbar</h1>
+              <div className="box-container">
+                <p className="box1">Ad 1</p>
+                <p className="box1">Ad 2</p>
               </div>
             </div>
           ) : null}
